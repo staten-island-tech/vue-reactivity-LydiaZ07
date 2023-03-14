c7a4641bc5e6fa5ce76226cec1c9bbea43cb8efb
LydiaZ07 <zhulydia07@gmail.com>
2023-03-14 13:15:25 -0400

    l

---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1 v-if="graduated">{{ student }}</h1>
+    <h1 v-else="graduated">They did not yet graduate</h1>  
+    <ul>
+      <li v-for="animal in animals" :key="animal">{{ animal }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Home", 
+  components: {},
+  data(){
+    return {
+      student: "Harry",
+      graduated: false,
+      animals: ["pig", "horse", "donkey", "capybara", "raccoon", "duck"],
+    };
+  },
+};
+
+</script>
+
+
+
+
