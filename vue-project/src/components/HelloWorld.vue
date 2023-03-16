597838755056ef5c79b423edd4d0bc754c0bc2df
LydiaZ07 <zhulydia07@gmail.com>
2023-03-16 13:13:59 -0400

    l

--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -2,11 +2,17 @@
   <div class="home">
     <h1 v-if="graduated">{{ student }}</h1>
     <h1 v-else="graduated">They did not yet graduate</h1>  
+    <h1 :class="titleClass">{{ message }}</h1>
+    <h2 v-if="latePractice">{{ message2 }}</h2>
+    <h2 v-else>cons- not going to practice at all with the lines that we put down, not use to own courts, far away, hungry</h2>
     <ul>
       <li v-for="animal in animals" :key="animal">{{ animal }}</li>
     </ul>
-    <button @click="authState" v-if="loggedIn">Log Out</button>
-    <button v-on:click="authState" v-else>Login</button>
+    <button @click="authState" v-if="loggedIn">Log Out</button>    <!-- @ = src, quicker way to use v-on -->
+
+    <button v-on:click="authState" v-else>Login</button>     <!-- v-on is how you run js in response to your dom events -->
+
+    <button @click="counts">count is {{ count }}</button>
   </div>
 </template>
 
@@ -21,6 +27,11 @@ export default {
       graduated: true,
       animals: ["pig", "horse", "donkey", "capybara", "raccoon", "duck"],
       loggedIn: false,
+      message: "hello world",
+      titleClass: "title",
+      count: 0,
+      latePractice: true,
+      message2: "pros- you can get food, you can study, don't need to travel to ps50, use the hardworking lines we put down, get use to our own school's courts",
     };
   },
   methods: {
@@ -31,11 +42,20 @@ export default {
       this.loggedIn = false;
     }
   },
-  }
+counts: function(){
+  this.count++
+},
+
+  },
 };
 
 </script>
 
+<style scoped>
+.title{
+  color:red;
+}
+</style>
 <!-- project should have 2 word components -->
 
 
